867057d469f7c35f5f094b194ef99898f550ac98
hegratrix <gratrixmomma@yahoo.com>
2018-12-28 01:04:38 -0800

    Updates

---
+++ precache-manifest.d853aa4babb58a9221990593e4d6cc97.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "11525fd074178a5559c0",
+    "url": "/Clicky/static/js/runtime~main.11525fd0.js"
+  },
+  {
+    "revision": "fe4f5551e3ccbd87c29b",
+    "url": "/Clicky/static/js/main.fe4f5551.chunk.js"
+  },
+  {
+    "revision": "346c24f384fa02c5a32f",
+    "url": "/Clicky/static/js/1.346c24f3.chunk.js"
+  },
+  {
+    "revision": "2fd5cf586897bb72d9b714a012c932aa",
+    "url": "/Clicky/index.html"
+  }
+];
